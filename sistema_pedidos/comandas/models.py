4584c51341b58d41d60af555297e7d09a2677b4c
João Tedeschi <joaorafaelbt@gmail.com>
2024-05-08 22:07:43 -0300

    feat: Update python version to 3.10 in GitHub Actions workflow

--- sistema_pedidos/comandas/models.py
+++ sistema_pedidos/comandas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Comanda(models.Model):
+    cliente = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+    
+
+class Barraca(models.Model):
+    nome = models.CharField(max_length=100)
+    comandas = models.ManyToManyField(Comanda)
+
+    def __str__(self):
+        return self.nome
