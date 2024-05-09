332433e10307502648d2e3e69de948e9531a6d00
João Tedeschi <joaorafaelbt@gmail.com>
2024-05-08 23:34:59 -0300

    Refactor views to include form handling for creating items and barracas

--- sistema_pedidos/comandas/views.py
+++ sistema_pedidos/comandas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Barraca, Item, OperadorBarraca, Pedido, ItemPedido
+from .forms import ItemPedidoForm, BarracaForm, PedidoForm, ItemForm, ItemPedidoFormSet, ItemFormSet, BarracaFormSet
 
 # Create your views here.
 def index(request):
@@ -18,10 +19,27 @@ def lista_pedidos(request):
     return render(request, 'comandas/lista_pedidos.html', {'pedidos': pedidos})
 
 def criar_item(request):
-    return render(request, 'comandas/criar_item.html')
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_itens')
+    else:
+        form = ItemForm()
+        formset = ItemFormSet()
+    return render(request, 'comandas/criar_item.html', {'form': form, 'formset': formset})
 
 def criar_barraca(request):
-    return render(request, 'comandas/criar_barraca.html')
+    if request.method == 'POST':
+        form = BarracaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_barracas')
+    else:
+        form = BarracaForm()
+        formset = BarracaFormSet()
+
+    return render(request, 'comandas/criar_barraca.html', {'form': form, 'formset': formset})
 
 def criar_pedido(request):
     return render(request, 'comandas/criar_pedido.html')
