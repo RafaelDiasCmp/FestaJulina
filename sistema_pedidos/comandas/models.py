6084a6cdfac13f5af9718432d91a88ecd0efc83b
João Tedeschi <joaorafaelbt@gmail.com>
2024-05-08 22:20:28 -0300

    feat: Add Pedido and ItemPedido models for managing orders and items in orders

--- sistema_pedidos/comandas/models.py
+++ sistema_pedidos/comandas/models.py
@@ -3,18 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Comanda(models.Model):
-    cliente = models.CharField(max_length=100)
-    valor = models.DecimalField(max_digits=5, decimal_places=2)
-
-    def __str__(self):
-        return self.nome
-    
-
 class Item(models.Model):
     nome = models.CharField(max_length=100)
     valor = models.DecimalField(max_digits=5, decimal_places=2)
-    comanda = models.ForeignKey(Comanda, on_delete=models.CASCADE)
+    barraca = models.ForeignKey('Barraca', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
@@ -24,8 +16,9 @@ class Item(models.Model):
         return self.user.username
 
 class Barraca(models.Model):
+    """ Barraca que vende itens """
     nome = models.CharField(max_length=100)
-    comandas = models.ManyToManyField(Comanda)
+    
 
 
 class OperadorBarraca(models.Model):
@@ -34,3 +27,21 @@ class OperadorBarraca(models.Model):
 
     def __str__(self):
         return self.nome
+    
+
+class Pedido(models.Model):
+    barraca = models.ForeignKey(Barraca, on_delete=models.CASCADE)
+    data_pedido = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Pedido {self.id}'
+
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, related_name='itens', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.quantidade} de {self.item.nome}'
+    
