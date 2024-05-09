c6d27407fac98953e9c64a8ed01ae168c5bf523e
João Tedeschi <joaorafaelbt@gmail.com>
2024-05-08 22:16:23 -0300

    feat: Add Item model to manage items in a comanda

--- sistema_pedidos/comandas/models.py
+++ sistema_pedidos/comandas/models.py
@@ -10,6 +10,15 @@ class Comanda(models.Model):
         return self.nome
     
 
+class Item(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=5, decimal_places=2)
+    comanda = models.ForeignKey(Comanda, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+
 class Barraca(models.Model):
     nome = models.CharField(max_length=100)
     comandas = models.ManyToManyField(Comanda)
